54cd1d91a00503326e15a75b930b2dc3c4377266
Sergii.Nagornyi <snagornyi@deloitte.com>
2019-12-04 15:26:38 +0200

    .

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 
+
 app.get("/api", function(req, res) {
-  res.send("Hello World!");
+  res.send("Hello World!"); 
 });
+
+app.use(express.static("public"));
 
 app.listen(8080, function() {
   console.log("Example app listening on port 8080!");
